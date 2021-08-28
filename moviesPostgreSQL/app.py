d534bd6960b413b11a0aa65ccf51e6d7a5086dbd
anatoli.kosarev <anatoli.kosarev@gmail.com>
2021-08-28 14:19:55 +0300

    .gitignore added

--- moviesPostgreSQL/app.py
+++ moviesPostgreSQL/app.py
@@ -4,7 +4,7 @@ import database
 menu = """Please select one of the following options:
 1) Add new movie.
 2) View upcoming movies.
-3) View all movies
+3) View all movies.
 4) Watch a movie.
 5) View watched movies.
 6) Create a new user.
